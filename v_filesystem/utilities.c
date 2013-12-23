271a3057ccf7c4f88109a871d7d4ca5bf3765f3e
WONDER <bingo@macbook.local>
2013-12-23 18:55:30 +0800

    add utilities.c and utilities.h to write some useful functions

---
+++ v_filesystem/utilities.c
@@ -0,0 +1,10 @@
+//
+//  utilities.c
+//  v_filesystem
+//
+//  Created by WONDER on 12/23/13.
+//  Copyright (c) 2013 WONDER. All rights reserved.
+//
+
+#include <stdio.h>
+#include "utilities.h"
---
+++ v_filesystem/utilities.h
@@ -0,0 +1,14 @@
+//
+//  utilities.h
+//  v_filesystem
+//
+//  Created by WONDER on 12/23/13.
+//  Copyright (c) 2013 WONDER. All rights reserved.
+//
+
+#ifndef v_filesystem_utilities_h
+#define v_filesystem_utilities_h
+
+
+
+#endif
