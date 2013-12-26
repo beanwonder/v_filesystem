1d69a9397e74aea61a04c272f9812db35b95865a
WONDER <bingo@macbook.local>
2013-12-27 00:29:37 +0800

    preliminary design of the primary data structure

--- v_filesystem/beanfs/beanfs.h
+++ v_filesystem/beanfs/beanfs.h
@@ -17,6 +17,7 @@
 // structure of the superblock on virtual
 // using logical block address from 0 to user defined number
 struct beanfs_super_block {
+    
     unsigned long s_blocks_count;
     unsigned long s_inodes_count;
     unsigned long s_free_blocks_count;
@@ -36,9 +37,9 @@ struct beanfs_super_block {
     unsigned long modify_time;                  // modified time stamp
 };
 
-
 // structure of superblock in memomry
 struct beanfs_sb_info {
+    
     unsigned long s_blocks_count;
     unsigned long s_inodes_count;
     unsigned long s_free_blocks_count;
@@ -59,14 +60,58 @@ struct beanfs_sb_info {
     unsigned long modify_time;                  // modified time stamp
 };
 
+#define BEANFS_NDADDR 4
+#define BEANFS_NINDIR 2
 // structure of inode on v_disk
 struct beanfs_inode {
+    
+    char bi_ftype;
+    unsigned short i_mode;                     // permission
+    unsigned short i_nlink;                    // file link count
+    unsigned long i_uid;                       // file onwer
+    unsigned long i_gid;                       // file owner group
+    
+    unsigned long i_atime;                     // last access time
+    unsigned long i_mtime;                     // last modify time
+    unsigned long i_ctime;                     // last inode changetime
+    unsigned long i_birthtime;                 // creation time
+    
+    unsigned long i_size;                      // file bytes count
+    unsigned long i_blocks;                    // actual blocks held
+    
+    struct {
+        unsigned long i_db[BEANFS_NDADDR];     // direct addr
+        unsigned long i_id[BEANFS_NINDIR];     // first is 1st direct and second is 2th indirect
+    } bi_addr;
     
 };
 
 // structure of inode in memory
 struct beanfs_inode_info {
     
+    unsigned long i_num;                       // inode number in memory
+    unsigned short i_access_count;             // access count
+    
+    char i_ftype;
+    unsigned short i_mode;                     // permission
+    unsigned short i_nlink;                    // file link count
+    unsigned long i_uid;                       // file onwer
+    unsigned long i_gid;                       // file owner group
+    
+    unsigned long i_atime;                     // last access time
+    unsigned long i_mtime;                     // last modify time
+    unsigned long i_ctime;                     // last inode changetime
+    unsigned long i_birthtime;                 // creation time
+    
+    unsigned long i_size;                      // file bytes count
+    unsigned long i_blocks;                    // actual blocks held
+    
+    struct {
+        unsigned long i_db[BEANFS_NDADDR];     // direct addr
+        unsigned long i_id[BEANFS_NINDIR];     // first is 1st direct and second is 2th indirect
+    } bi_addr;
+    
+    unsigned short inode_lock;                 // 0 : unlocked and 1 : locked
 };
 
 
